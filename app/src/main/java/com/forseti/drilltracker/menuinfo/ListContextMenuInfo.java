c28a21e87166c605eb5a7c88faa168bc1ae010ff
Sarah <sarahforseti@gmail.com>
2016-03-28 16:36:37 -0500

    Removing edit menu for now

--- app/src/main/java/com/forseti/drilltracker/menuinfo/ListContextMenuInfo.java
+++ app/src/main/java/com/forseti/drilltracker/menuinfo/ListContextMenuInfo.java
@@ -5,16 +5,10 @@ import android.view.ContextMenu;
 import android.widget.ExpandableListView;
 
 import com.forseti.drilltracker.adapter.ExpandableDrillListAdapter;
-import com.forseti.drilltracker.clicklistener.CategoryContextMenuClickListener;
 import com.forseti.drilltracker.clicklistener.CategoryDeleteMenuListener;
-import com.forseti.drilltracker.clicklistener.DrillContextMenuClickListener;
 import com.forseti.drilltracker.clicklistener.DrillDeleteMenuListener;
 
 public class ListContextMenuInfo {
-    public final static int CATEGORY_EDIT = 0;
-    public final static int CATEGORY_DELETE = 1;
-    public final static int DRILL_EDIT = 2;
-    public final static int DRILL_DELETE = 3;
 
     public static void createMenu(Context context, ContextMenu menu, ExpandableListView.ExpandableListContextMenuInfo info, ExpandableDrillListAdapter listAdapter) {
         int category = ExpandableListView.getPackedPositionGroup(info.packedPosition);
@@ -22,13 +16,9 @@ public class ListContextMenuInfo {
         int drillPosition = ExpandableListView.getPackedPositionChild(info.packedPosition);
 
         if (type == ExpandableListView.PACKED_POSITION_TYPE_GROUP) {
-            CategoryContextMenuClickListener categoryContextMenuClickListener = new CategoryContextMenuClickListener(context, listAdapter);
-            menu.add(category, drillPosition, 1, "Edit Category Name").setOnMenuItemClickListener(categoryContextMenuClickListener);
             menu.add(category, drillPosition, 2, "Delete Category").setOnMenuItemClickListener(new CategoryDeleteMenuListener(context, listAdapter));
         }
         else {
-            DrillContextMenuClickListener drillContextMenuClickListener = new DrillContextMenuClickListener(context, listAdapter);
-            menu.add(category, drillPosition, 1, "Edit Drill").setOnMenuItemClickListener(drillContextMenuClickListener);
             menu.add(category, drillPosition, 2, "Delete Drill").setOnMenuItemClickListener(new DrillDeleteMenuListener(context, listAdapter));;
         }
     }
